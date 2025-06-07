204444a93e0dc0aa1b512925ecc32f465ebc0fbb
JaeYeul Jhang <jaeyeuljhang@JaeYeuls-MacBook-Pro.local>
2025-06-07 19:52:15 +0900

    adf

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /var/www/html/index.php ]; then
-    echo "📦 초기 WordPress 파일 복사 중..."
-    id
-    ls -alh /var/www/html
-    touch /var/www/html/asdfasd
-    cp -r /tmp/wordpress/* /var/www/html/
-else
-    echo "✅ WordPress 파일이 이미 존재합니다. 복사 생략."
+if [ ! -f wp-config.php ]; then
+    echo "[INFO] wp-config.php not found. Running wp core install setup..."
+
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${MYSQL_PASSWORD}" \
+        --dbhost="mariadb" \
+        --path="/var/www/html" \
+        --allow-root
+
+    wp core install \
+        --url="https://${DOMAIN_NAME}" \
+        --title="Inception WordPress" \
+        --admin_user="admin" \
+        --admin_password="adminpass" \
+        --admin_email="admin@example.com" \
+        --skip-email \
+        --path="/var/www/html" \
+        --allow-root
 fi
 
 exec "$@"
